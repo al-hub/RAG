cea99c44b57c8897500a11c876f173a135788aa3
al-hub <56526241+al-hub@users.noreply.github.com>
2024-04-25 14:22:01 +0900

    Create Multimodal_Speech_Processing.m

---
+++ Multimodal_Speech_Processing.m
@@ -0,0 +1,19 @@
+## 딥러닝
+ - E2E (featuer포함) 경향  
+ - Self-supervisied learning : pretext task / proxy task 필요 
+   - knowledge transfer
+   - Contrastive learning - SimCLR
+   - Self-Supervised 예시 : 소 그림 + 음매 소리 
+
+## Multi-modal learning
+ - idea 사람 두뇌 responise 가 비슷했다.
+ - 동영상 : 오디오+비디오 같은 의미를 가져올 수 있음 (Coressponde 가 있다.)
+ - Natural Coresspondense
+ - Identity 정보, Content 정보  
+
+## 음성인식 관련 & 음성 신호처리 방향
+ - LibriSppech : 무료DB 매우 clean, WER 약 1.4% 내외 But 실재 환경에서는.... 81.3% (CHiMe
+ - Audio-Visual Speech enhancement
+ - 단일 정보보다 다중 정보로 진행하는 방식
+ - 사용자의 몰입도를 높이는 기술 
+ 
